6bef86f8dd8e7171127ef1c698af63ffaf57f76c
cnickel184 <128544032+cnickel184@users.noreply.github.com>
2024-03-01 11:40:17 -0800

    modified:   src/pages/Tab1.css 	modified:   src/pages/Tab3.css 	modified:   src/pages/Tab3.tsx 	modified:   src/pages/signup.css

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -2,6 +2,7 @@ import { Bar } from "react-chartjs-2";
 import { CategoryScale } from "chart.js";
 import { Chart as ChartJS } from "chart.js/auto";
 import {
+  IonPage,
   IonTitle,
   useIonViewWillEnter,
   useIonViewWillLeave,
@@ -17,26 +18,28 @@ const Graph: React.FC = () => {
   }, []);
 
   const data = {
-    labels: ["Billable", "Non Billable"],
+    labels: ["Billable", "Non Billable"],/*change to show the 7ish most resent dates */
     
     datasets: [
       {
-        label: "Net gain",/*change to show the 7ish most resent dates */
-        backgroundColor: "#53a45f",
+        label: "Net gain",
+        
+        backgroundColor: "#206b52",
         borderColor: "#102A43",
         borderWidth: 1,
         hoverBackgroundColor: "#8595bc",
         hoverBorderColor: "#206b52",
-        data: [65, 59], /* change to rep the 7ish most recent values of net gain/loss*/
+        
+        data: [65, -59], /* change to rep the 7ish most recent values of net gain/loss*/
       },
     ],
   };
 
   return (
-    <>
-      <IonTitle>Net Gain </IonTitle>
+    <IonPage>
+      <IonTitle className="thing">Net Gain </IonTitle>
       <Bar className="brand" data={data} />
-    </>
+    </IonPage>
   );
 };
 
